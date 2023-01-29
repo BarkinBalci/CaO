5764ba84a9ccef56ecbda58262075bf177771752
Barkın Balcı <62614933+BarkinBalci@users.noreply.github.com>
2023-01-30 01:05:23 +0300

    feat: display func for numpad

--- src/main.js
+++ src/main.js
@@ -2,10 +2,15 @@ const { invoke } = window.__TAURI__.tauri;
 
 let calculateInput;
 let resultMsg;
+let testButton;
 
 async function calculate() {
   // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
   resultMsg.textContent = await invoke("calculate", { input: calculateInput.value });
+}
+
+async function display(val) {
+  calculateInput.value += val;
 }
 
 window.addEventListener("DOMContentLoaded", () => {
